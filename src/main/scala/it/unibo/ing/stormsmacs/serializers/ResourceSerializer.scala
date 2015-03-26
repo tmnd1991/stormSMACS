c1cb064660baa1b7cf6e3490f10c0c654d375166
Antonio Murgia <tmnd91@gmail.com>
2015-03-26 03:28:18 +0100

    tuned performance

--- src/main/scala/it/unibo/ing/stormsmacs/serializers/ResourceSerializer.scala
+++ src/main/scala/it/unibo/ing/stormsmacs/serializers/ResourceSerializer.scala
@@ -8,6 +8,7 @@ import com.esotericsoftware.kryo.{Kryo, Serializer}
 import com.twitter.chill.ScalaKryoInstantiator
 import org.openstack.api.restful.ceilometer.v2.elements.Resource
 import org.openstack.api.restful.elements.Link
+import scala.collection.JavaConverters._
 
 /**
  * Created by tmnd91 on 25/03/15.
@@ -18,10 +19,11 @@ class ResourceSerializer extends Serializer[Resource]{
   import org.openstack.api.restful.ceilometer.v2.elements.JsonConversions._
   val instantiator = new ScalaKryoInstantiator()
   val myKryo = instantiator.newKryo()
+
   override def write(kryo: Kryo, output: Output, t: Resource): Unit = {
     myKryo.writeObject(output,if (t.first_sample_timestamp.isDefined) Some(t.first_sample_timestamp.get.getTime) else None)
     myKryo.writeObject(output,if (t.last_sample_timestamp.isDefined) Some(t.last_sample_timestamp.get.getTime) else None)
-    myKryo.writeObject(output,t.links.toArray)
+    kryo.writeObject(output,t.links.toArray)
     myKryo.writeObject(output,t.metadata)
     myKryo.writeObject(output,t.project_id)
     myKryo.writeObject(output,t.resource_id)
@@ -34,7 +36,7 @@ class ResourceSerializer extends Serializer[Resource]{
     val lastSample = myKryo.readObject(input,classOf[Some[Long]])
     Resource( first_sample_timestamp = (if (firstSample.isEmpty) Some(new Timestamp(firstSample.get)) else None),
               last_sample_timestamp  = (if (firstSample.isEmpty) Some(new Timestamp(lastSample.get)) else None),
-              links = myKryo.readObject(input,classOf[Array[Link]]),
+              links = kryo.readObject(input,classOf[Array[Link]]),
               metadata = myKryo.readObject(input, classOf[Map[String,String]]),
               project_id = myKryo.readObject(input, classOf[Some[String]]),
               resource_id = myKryo.readObject(input, classOf[String]),
