ea29d60c857379c0ff1b5a19d9f438feec8de51a
Antonio Murgia <tmnd91@gmail.com>
2015-04-18 01:31:23 +0200

    that can be reliability....

--- src/main/scala/it/unibo/ing/stormsmacs/topologies/bolts/CloudFoundryNode/Typed/CloudFoundryNodeClientBolt.scala
+++ src/main/scala/it/unibo/ing/stormsmacs/topologies/bolts/CloudFoundryNode/Typed/CloudFoundryNodeClientBolt.scala
@@ -8,6 +8,7 @@ import it.unibo.ing.stormsmacs.topologies.bolts.Typed.HttpRequesterBolt
 import storm.scala.dsl.additions.Logging
 import spray.json._
 import java.util.Date
+import it.unibo.ing.utils._
 /**
  * @author Antonio Murgia
  * @version 24/12/14
@@ -17,29 +18,21 @@ class CloudFoundryNodeClientBolt(node : CloudFoundryNodeConf)
   extends HttpRequesterBolt(node.connectTimeout, node.readTimeout, false,"Node","GraphName","MonitData")
   with Logging
 {
-
-  override def execute(t: Tuple) = {
-    try{
-      t matchSeq{
-        case Seq(date : Date) =>{
-          val response = httpClient.doGET(uri = node.url.toURI, timeout = node.readTimeout)
-          if (response isSuccess){
-            import spray.json.DefaultJsonProtocol._
-            val data = response.content.parseJson.convertTo[Seq[MonitInfo]]
-            for (d <- data)
-              using anchor t emit (node, date, d)
-            t ack
-          }
-          else{
-            t fail
-          }
+  override def execute(t: Tuple) = t matchSeq{
+    case Seq(date : Date) =>{
+      try {
+        val response = httpClient.doGET(uri = node.url.toURI / date.getTime.toString, timeout = node.readTimeout)
+        if (response isSuccess) {
+          import spray.json.DefaultJsonProtocol._
+          val data = response.content.parseJson.convertTo[Seq[MonitInfo]]
+          for (d <- data)
+            using anchor t emit(node, date, d)
         }
-        case x => logger.error("invalid input tuple: expected Date and " + x + "found")
       }
-    }
-    catch{
-      case e : Throwable => {
-        logger.trace("", e)
+      catch{
+        case e : Throwable => logger.trace("", e)
+      }
+      finally{
         t ack
       }
     }
--- src/main/scala/it/unibo/ing/stormsmacs/topologies/bolts/GenericNode/Typed/GenericNodeClientBolt.scala
+++ src/main/scala/it/unibo/ing/stormsmacs/topologies/bolts/GenericNode/Typed/GenericNodeClientBolt.scala
@@ -7,6 +7,7 @@ import it.unibo.ing.sigar.restful.model.SigarMeteredData
 import it.unibo.ing.stormsmacs.conf.GenericNodeConf
 import it.unibo.ing.stormsmacs.topologies.bolts.Typed.HttpRequesterBolt
 import spray.json._
+import it.unibo.ing.utils._
 import it.unibo.ing.sigar.restful.model.SigarMeteredDataFormat._
 import storm.scala.dsl.additions.Logging
 
@@ -17,57 +18,24 @@ import storm.scala.dsl.additions.Logging
  */
 class GenericNodeClientBolt(val node : GenericNodeConf)
   extends HttpRequesterBolt(node.connectTimeout, node.readTimeout, false, "Node","GraphName","MonitData")
-  //extends HttpRequesterBolt[Tuple1[Date], (GenericNodeConf, Date, SigarMeteredData)]
   with Logging
 {
-  /*
-  override def typedExecute(t: Tuple1[Date], st : Tuple): Unit = {
-    try{
-      val exchange = new ContentExchange()
-      exchange.setURI(node.url.toURI)
-      exchange.setMethod("GET")
-      httpClient.send(exchange)
-      val state = exchange.waitForDone()
-      val body = exchange.getResponseContent
-      using anchor st emit (node, t._1, body.parseJson.convertTo[SigarMeteredData])
-      //st.ack
-    }
-    catch{
-      case e : Throwable => {
-        logger.error(e.getStackTrace.mkString("\n"))
-        //st.ack //the replay of this is fruitless, because the value will be reread in the future and wouldn't correspond to the original one
+  override def execute(t: Tuple) : Unit = t matchSeq {
+    case Seq(date: Date) =>{
+      try{
+        val data = httpClient.doGET(node.url.toURI / date.getTime.toString, node.readTimeout)
+        if (data.isSuccess)
+          using anchor t emit (node, date, data.content.parseJson.convertTo[SigarMeteredData])
+        else
+          logger error "response code not successful"
       }
-    }
-    finally {
-      st.ack
-    }
-  }
-  */
-  override def execute(t: Tuple): Unit = {
-    try{
-      t matchSeq {
-        case Seq(date: Date) =>{
-          val data = httpClient.doGET(node.url.toURI, node.readTimeout)
-          if (data.isSuccess)
-            using anchor t emit (node, date, data.content.parseJson.convertTo[SigarMeteredData])
-          else
-            logger.error("response code not successfull")
-        }
-        case x => {
-          logger.error("invalid input tuple: expected Date and " + x + "found")
-          t fail
-        }
+      catch{
+        case e : Throwable => logger.trace(e.getMessage,e)
+
       }
-      httpClient.doGET(node.url.toURI)
-    }
-    catch{
-      case e : Throwable => {
-        logger.trace("",e)
-        //the replay of this is fruitless, because the value will be reread in the future and wouldn't correspond to the original one
+      finally {
+        t ack
       }
-    }
-    finally {
-      t ack
     }
   }
 }
