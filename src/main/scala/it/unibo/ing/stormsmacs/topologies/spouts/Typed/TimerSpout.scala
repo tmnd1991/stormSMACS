ae90bc4a801b20f043cb96b29bfa8ac7fd847e61
Antonio Murgia <tmnd91@gmail.com>
2015-04-18 20:24:25 +0200

    fixing to see what the error is

--- src/main/scala/it/unibo/ing/stormsmacs/topologies/spouts/Typed/TimerSpout.scala
+++ src/main/scala/it/unibo/ing/stormsmacs/topologies/spouts/Typed/TimerSpout.scala
@@ -1,6 +1,7 @@
 package it.unibo.ing.stormsmacs.topologies.spouts.Typed
 
 import backtype.storm.tuple.MessageId
+import storm.scala.dsl.additions.Logging
 import storm.scala.dsl.{StormSpout}
 
 import backtype.storm.utils.Utils
@@ -12,11 +13,14 @@ import java.util.Date
  * Spout that gives the clock to the entire stormsmacs topology
  */
 
-class TimerSpout(pollTime : Long) extends StormSpout(List("GraphName")){
+class TimerSpout(pollTime : Long) extends StormSpout(List("GraphName")) with Logging{
   override def nextTuple = {
     Utils.sleep(pollTime)
     val now = new Date()
     using msgId(now.getTime) emit (now)
+  }
+  override def ack(messageId: Any): Unit = {
+    logger info "acked " + new Date(messageId.asInstanceOf[Long])
   }
   override def fail(messageId: Any) : Unit = {
     val dateToBeReplayed = new Date(messageId.asInstanceOf[Long])
