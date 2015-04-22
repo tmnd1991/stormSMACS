77bc423c79aa700c1a076a781ae9e9b1cba0e5e3
Antonio Murgia <tmnd91@gmail.com>
2015-04-22 02:51:29 +0200

    too many logs

--- src/main/scala/it/unibo/ing/stormsmacs/topologies/reliable/spouts/TimerSpout.scala
+++ src/main/scala/it/unibo/ing/stormsmacs/topologies/reliable/spouts/TimerSpout.scala
@@ -1,11 +1,16 @@
 package it.unibo.ing.stormsmacs.topologies.reliable.spouts
 
+import java.util.concurrent.{LinkedBlockingQueue, BlockingQueue, TimeUnit, Executors}
+import scala.concurrent._
+import ExecutionContext.Implicits.global
 import it.unibo.ing.stormsmacs.topologies.facilities.{FailHandler, DefaultFailHandler}
 import storm.scala.dsl.additions.Logging
 import storm.scala.dsl.{StormSpout}
 
-import backtype.storm.utils.Utils
+
 import java.util.Date
+
+
 
 /**
  * @author Antonio Murgia
@@ -15,12 +20,18 @@ import java.util.Date
 
 class TimerSpout(pollTime : Long) extends StormSpout(List("GraphName")) with Logging{
   private var failHandler : FailHandler[Long] = _
+  private var queue : BlockingQueue[Long] = _
   setup{
     failHandler = new DefaultFailHandler(2)
+    queue = new LinkedBlockingQueue[Long]()
+    val runnable = new Runnable() {
+      override def run() = queue.put(System.currentTimeMillis())
+    }
+    val service = Executors.newSingleThreadScheduledExecutor()
+    service.scheduleAtFixedRate(runnable, 0, pollTime, TimeUnit.MILLISECONDS)
   }
-  override def nextTuple = {
-    Utils.sleep(pollTime)
-    val now = new Date()
+  override def nextTuple = Future{
+    val now = new Date(queue.take())
     using.msgId(now.getTime).emit(now)
   }
   override def ack(messageId: Any): Unit = {
