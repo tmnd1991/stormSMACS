954e9725d47df5a7b17b02d6907d77cccf85d868
Antonio Murgia <tmnd91@gmail.com>
2015-04-26 10:36:39 +0200

    final version

--- src/main/scala/it/unibo/ing/stormsmacs/topologies/reliable/bolts/OpenStack/OpenStackSampleBolt.scala
+++ src/main/scala/it/unibo/ing/stormsmacs/topologies/reliable/bolts/OpenStack/OpenStackSampleBolt.scala
@@ -6,9 +6,11 @@ import java.util.Date
 import backtype.storm.tuple.Tuple
 import it.unibo.ing.stormsmacs.conf.OpenStackNodeConf
 import org.openstack.api.restful.ceilometer.v2.elements.{Sample, Resource}
-import org.openstack.clients.ceilometer.v2.CeilometerClient
+import org.openstack.clients.ceilometer.v2.{ICeilometerClient, CeilometerClient}
 import storm.scala.dsl.additions.Logging
 import storm.scala.dsl.StormBolt
+
+import scala.collection.mutable
 
 /**
  * @author Antonio Murgia
@@ -18,9 +20,10 @@ import storm.scala.dsl.StormBolt
 class OpenStackSampleBolt(pollTime: Long)
   extends StormBolt(List("NodeName", "GraphName", "Resource", "Sample"))
   with Logging {
+  private val _clients: mutable.Map[String, ICeilometerClient] = _
   override def execute(input: Tuple) = input matchSeq {
       case Seq(node: OpenStackNodeConf, date: Date, resource: Resource) =>
-        val cclient = CeilometerClient.getInstance(node.ceilometerUrl, node.keystoneUrl, node.tenantName, node.username, node.password, node.connectTimeout, node.readTimeout)
+        val cclient = getClient(node.ceilometerUrl, node.keystoneUrl, node.tenantName, node.username, node.password, node.connectTimeout, node.readTimeout)
         val start = new Date(date.getTime - pollTime)
         val maybeSamples = cclient.tryGetSamplesOfResource(resource.resource_id, start, date)
         maybeSamples match {
@@ -35,5 +38,11 @@ class OpenStackSampleBolt(pollTime: Long)
             logger.error(s"cannot get samples of ${resource.resource_id} from $start to $date")
             input.fail //if we get None as a result, something bad happened, we need to replay the tuple
         }
+  }
+  private def getClient(ceilometerUrl : URL, keystoneUrl : URL, tenantName : String,  username : String, password : String, connectTimeout: Int, readTimeout: Int) : ICeilometerClient = {
+    val key = ceilometerUrl+tenantName+username+password
+    if (!(_clients contains key))
+      _clients(key) = new CeilometerClient(ceilometerUrl, keystoneUrl, tenantName, username, password, connectTimeout, readTimeout)
+    _clients(key)
   }
 }
